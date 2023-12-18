e0a08e89f8e4436626a71a62d1cf1aeba05d2be4
Lorena Martinez Garcia <lmartinezgarcia7@uoc.edu>
2023-12-18 16:05:34 +0100

    Cambios script nextflow

--- run_ampliseq.sh
+++ run_ampliseq.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#Submuestreo de los reads a partir de un script de R, con tres proporciones (0.25,0.5,0.75)
+#Submuestreo de los reads a partir de submuestreo.R, genera cuatro escenarios de profundidad de secuenciación (alto número de lecturas, bajo número de lecturas, balanceado y desigual.
 # Rutas de los directorios base
-INPUT_DIRECTORY="/home/lorena/TFM/muestras/100Percent"
-OUTPUT_DIRECTORY="/home/lorena/TFM/muestras"
+INPUT_DIRECTORIO="/home/lorena/TFM/100Percent"
+OUTPUT_DIRECTORIO="/home/lorena/TFM/muestras"
 
 # Se llama al script de R, pasando las rutas de los directorios
-Rscript /home/lorena/TFM/submuestreo_reads.R $INPUT_DIRECTORY $OUTPUT_DIRECTORY
+Rscript /home/lorena/TFM/submuestreo.R $INPUT_DIRECTORIO $OUTPUT_DIRECTORIO
 
 # Nf-core/ampliseq
 # Se definen  los directorios base que va a coger nf-core/ampliseq
@@ -16,28 +16,32 @@ BASE_OUTPUT_DIR="/home/lorena/TFM/resultados"
 FW_PRIMER="TCCTACGGGAGGCAGCAGT"
 RV_PRIMER="GGACTACCAGGGTATCTAATCCTGTT"
 PROFILE="docker"
+#Crear el directorio de resultados de Nextflow si no existe, crearlo
 
-# Bucle para que se ejecute nf-core/ampliseq sobre cada directorio de las distintas proporciones
-for PROP_DIR in "$BASE_INPUT_DIR"/*Percent; do
-  if [ -d "$PROP_DIR" ]; then
-    # se extrae la propoción del nombre base
-    PROP_PERCENT=$(basename "$PROP_DIR")
-    
-    # Se define el directorio de salida para la proporción Define the output directory for this proportion
-    OUTDIR="$BASE_OUTPUT_DIR/$PROP_PERCENT"
-    
-    # Nos aseguramos de que existe el directorio de salida
-    mkdir -p "$OUTDIR"
-    
+mkdir -p "$BASE_OUTPUT_DIR"
+
+for ESCENARIO_DIR in "$BASE_INPUT_DIR"/*_reads; do
+  if [ -d "$ESCENARIO_DIR" ]; then
+    # Extraer el nombre del escenario
+    ESCENARIO_NAME=$(basename "$ESCENARIO_DIR")
+
+    # Definir el directorio de salida para este escenario en Nextflow
+    NF_ES_OUTDIR="$BASE_OUTPUT_DIR/$SCENARIO_NAME"
+
     # Se corre nf-core/ampliseq
     nextflow run nf-core/ampliseq \
         -r 2.7.1 \
         -profile "$PROFILE" \
-        --input_folder "$PROP_DIR" \
+        --input_folder "$ESCENARIO_DIR" \
         --FW_primer "$FW_PRIMER" \
         --RV_primer "$RV_PRIMER" \
-        --outdir "$OUTDIR"  \
-        --max_memory '8.GB'
+        --outdir "$NF_ES_OUTDIR"  \
+        --max_memory '8.GB'  \
+        --max_time "1.h" \
+       
 
+
+        
   fi
 done
+
